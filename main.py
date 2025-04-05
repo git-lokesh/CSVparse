2736d6c64087dbf7ddf57f15099fb96cd2cfc354
lokesh <lokeshofficial2212@gmail.com>
2025-04-05 12:55:47 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as p
+
+d1 = p.read_csv("learn.csv")
+d2 = p.read_csv("data.csv")
+# print("BASIC INFO : ")
+# print(d2.info())
+# print("DESCRIBE  :")
+print(d2['Annual_Income_(k$)'].mean())
+print(d2.isnull().sum())
