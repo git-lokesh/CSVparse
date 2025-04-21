2f17b0cd0ad27c511e39719d43fbced490cfe416
lokesh <lokeshofficial2212@gmail.com>
2025-04-21 19:49:06 +0530

    new PC

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as p
 d1 = p.read_csv("learn.csv")
 d2 = p.read_csv("data.csv")
 # print("BASIC INFO : ")
-# print(d2.info())
+print(d2.info())
 # print("DESCRIBE  :")
 print(d2['Annual_Income_(k$)'].mean())
 print(d2.isnull().sum())
